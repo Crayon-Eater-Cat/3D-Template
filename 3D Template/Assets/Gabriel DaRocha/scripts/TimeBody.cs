e92ad12a8d4097f4490d2be0170131ea804b9cf8
gabrielrocha2007brasil <gabrielrocha2007brasil@gmail.com>
2025-02-27 10:18:38 -0800

    timeTravel,

--- 3D Template/Assets/Gabriel DaRocha/scripts/TimeBody.cs
+++ 3D Template/Assets/Gabriel DaRocha/scripts/TimeBody.cs
@@ -1,18 +1,77 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TimeBody : MonoBehaviour
 {
-    public bool isRewinding = false;
+    private bool isRewinding = false;
+
+    public float recordTime = 5f;
+
+    List<PointInTime> pointsInTime;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) { 
-        isRewinding =true;
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            StartRewind();
         }
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            StopRewind();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else {
+            Record();
+        }
+    }
+    void Record()
+    {
+        if (pointsInTime.Count >  Mathf.Round(recordTime / Time.fixedDeltaTime))  // 5 seconds
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
+            pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+        
+        }
+
+    void Rewind()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        else
+        {
+        StopRewind ();
+        }
+    }
+
+    public void StartRewind()
+    {
+        isRewinding=true;
+        rb.isKinematic = true;
+    }
+  public  void StopRewind()
+    {
+        isRewinding=false;
+        rb.isKinematic = false;
     }
 }
